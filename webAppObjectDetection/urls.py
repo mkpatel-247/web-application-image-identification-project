95a0f7d95b9b8bb41d41ffb6f744fb72c55832ec
mkpatel-247 <meet.codes247@gmail.com>
2024-09-04 00:57:51 +0530

    Created the flow of image upload.

--- webAppObjectDetection/urls.py
+++ webAppObjectDetection/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('imageUpload/', include('imageUpload.urls')),
+    path('imageupload/', include('imageUpload.urls')),
 ]
