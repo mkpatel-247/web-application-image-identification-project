0ae426defee2ae00566f918d5c53fb0be8ede4b2
mkpatel-247 <meet.codes247@gmail.com>
2024-09-06 00:35:47 +0530

    Improved design with bootstrap.

--- imageUpload/views.py
+++ imageUpload/views.py
@@ -15,26 +15,31 @@ def home(request):
     return render(request, 'home.html')
 
 def imageProcess(request):
-    form = ImageUploadForm(request.POST, request.FILES)
-    if form.is_valid():
-        handle_uploaded_file(request.FILES['image'])
+    print("RESPIQASD...", request.FILES)
+    if request.FILES: 
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['image'])
 
-        model = ResNet50(weights='imagenet')
-        img_path = 'img.jpg'
+            model = ResNet50(weights='imagenet')
+            img_path = 'img.jpg'
 
-        img = image.load_img(img_path, target_size=(224, 224))
-        x = image.img_to_array(img)
-        x = np.expand_dims(x, axis=0)
-        x = preprocess_input(x)
+            img = image.load_img(img_path, target_size=(224, 224))
+            x = image.img_to_array(img)
+            x = np.expand_dims(x, axis=0)
+            x = preprocess_input(x)
 
-        preds = model.predict(x)
+            preds = model.predict(x)
 
-        print('Predicted: ', decode_predictions(preds, top=3)[0])
-        html = decode_predictions(preds, top=3)[0]
-        res = []
+            print('Predicted: ', decode_predictions(preds, top=3)[0])
+            html = decode_predictions(preds, top=3)[0]
+            res = []
 
-        for e in html:
-            res.append((e[1], np.round(e[2]*100, 2)))
-        return render(request, 'result.html', {'res':res})
-        
-        return render(request, 'result.html')
+            for e in html:
+                res.append((e[1], np.round(e[2]*100, 2)))
+            return render(request, 'result.html', {'res':res})
+    else:
+        return render(request, 'home.html')
+    return render(request, 'result.html')
+
+    
